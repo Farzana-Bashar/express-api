935dfea229fdfdb1ba53cc45f9d712839d75477c
Farzana Bashar <farzana@Farzanas-MacBook-Pro.local>
2021-04-05 20:30:12 +0600

    add userPost endpoint

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ const express= require('express');
 
 const app= express();
 
-app.get("/api/v1", (req,res)=>{
-    res.send('Hello there!!');
+app.use(express.json());
+
+var data={};
+
+app.post("/api/user", (req,res)=>{
+    data=req.body;
+    console.log(data);
+    res.send(data);
 })
+
+app.get("/api/v1", (req,res)=>{
+    res.send(data);
+})
+
 
 const PORT= 3000;
 
